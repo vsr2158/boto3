ae8cbccd852dc46e459ab479e82480bba7865319
vsr2158 <v.shekhar@yahoo.com>
2021-05-08 22:31:29 -0700

    first commit

--- s3get-ebsput.py
+++ s3get-ebsput.py
@@ -1,18 +1,28 @@
 import boto3
-import datetime as datetime
-BUCKET_NAME = "137965528627-s3get-ebsput"
-OBJECT_NAME = 'file1'
-FILE_NAME = OBJECT_NAME
+import random
+import threading
+import time
 
-def s3_get (BUCKET_NAME,OBJECT_NAME):
+bucket = "137965528627-s3get-ebsput"
+object_name = 'file'
+file_name = object_name
+
+def s3_get (file_name, bucket, object_name):
     s3 = boto3.client('s3')
+    r = str(random.randrange(1,1000))
+    object_name = object_name + r
+    file_name = object_name
     try:
-        s3.download_file('BUCKET_NAME', 'OBJECT_NAME', str(current_utc_time))
+        s3.download_file(bucket,object_name, file_name)
         return True
     except FileNotFoundError:
         print("The file was not found")
         return False
 
-current_utc_time = datetime.datetime.utcnow()
-s3_get(BUCKET_NAME, OBJECT_NAME)
+try:
+    for i in range (25):
+        threading.Thread(target=s3_get(file_name,bucket,object_name))
+
+except:
+   print ("Error: unable to start thread")
 
